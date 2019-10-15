7dddc00b67e865f59b040177955b236656c66d58
hayden <haydentao@163.com>
2019-10-15 18:08:35 +0800

    v0.1---基本框架和首页展示开发完毕

---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav-bar class="title">
+      <div slot="center">详情页</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "../../../components/common/navbar/NavBar";
+  export default {
+    name: "DetailNavBar",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+      }
+    },
+  }
+
+</script>
+
+<style scoped>
+</style>
